9536597dade743e0756afa4dd8941bf54b8b1720
Jun <cmoonjun11@gmail.com>
2023-03-21 15:10:50 +0900

    [프로젝트 기초공사1] 이미지 애셋 세팅

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
-import RouterTest from './components/RouterTest';
+
 
 function App() {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
+
   return (
     <BrowserRouter>
       <div className="App">
         <h2>App.js</h2>
+
+        {/* 
+          process.env.PUBLIC_URL는 public 디렉토리의 경로를 
+          바로 쓸 수 있는 명령어다!
+        */}
+        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} />
+
+
         <Routes>
           <Route path="/" element={ <Home /> } />
           <Route path='/new' element={ <New />} />
           <Route path='/edit' element={ <Edit />} />
           <Route path='/diary/:id' element={ <Diary />} />
         </Routes>
-        <RouterTest/>
+
       </div>
     </BrowserRouter>
   );
